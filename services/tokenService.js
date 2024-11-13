4354c5a9dd53df0635317b0279c5bf527b32c277
Victor C <victorcamacaro253@gmail.com>
2024-11-13 15:46:44 -0400

    Add files via upload

---
+++ services/tokenService.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const generateToken=(UserId,email,role)=>{
+   
+  // Generar un token JWT
+  const token = jwt.sign(
+    { id: UserId,email:email,role:role},
+    process.env.JWT_SECRET, // Asegúrate de tener JWT_SECRET en tus variables de entorno
+    { expiresIn: '1h' } // Expiración del token, por ejemplo, 1 hora
+);
+
+return token;
+} 
+
+const verifyToken=(token)=>{
+     try{
+
+  const decode = jwt.verify(token, process.env.JWT_SECRET);
+  return decode.id; //Retorna el id del usuario 
+}catch(error){
+    return null  
+  }
+
+}
+
+export default {generateToken,verifyToken }
