fb338ea061b9e3f16c960b7adfcf314aef658f5a
Victor C <victorcamacaro253@gmail.com>
2024-11-13 15:46:34 -0400

    Add files via upload

---
+++ middleware/multer.js
@@ -0,0 +1,22 @@
+import multer from "multer";
+import path from "path";
+
+
+//Configuracion de almacenamiento
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+        },
+        filename:(req,file,cb)=>{
+         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+         cb(null, uniqueSuffix + path.extname(file.originalname))
+        }
+
+    })
+
+
+    //Inicializar Multer
+    const upload = multer({ storage: storage });
+
+    export default upload
