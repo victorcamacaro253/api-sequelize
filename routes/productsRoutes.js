74cb9e17672bcde2eb00b5add219763fa07ef11d
Victor C <victorcamacaro253@gmail.com>
2024-11-13 15:45:22 -0400

    Add files via upload

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,13 +1,19 @@
 import {Router} from "express";
 import productsController from "../controllers/productsControllers.js";
-
+import upload from "../middleware/multer.js";
 
 const router=  Router()
 
 
 router.get('/',productsController.getProducts)
 
+router.get('/name',productsController.getProductByName)
+
 router.get('/:id',productsController.getProductById)
+
+router.post('/addMultipleProducts',upload.array('image'),productsController.addMultipleProducts)
+
+router.delete('/:id',productsController.deleteProduct)
 
 
 export default router
