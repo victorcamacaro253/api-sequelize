f492ae4246316ee7f09b0040b1a060b090b4516f
Victor C <victorcamacaro253@gmail.com>
2024-11-14 15:47:51 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import userRoutes from  "./routes/userRoutes.js";
 import morgan from "morgan";
 import cors from 'cors'
 import productsRoutes from "./routes/productsRoutes.js";
+import comprasRoutes from './routes/comprasRoutes.js'
+import helmet from "helmet";
+
 
 const app =express()
 
@@ -12,12 +15,13 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.json({message:'hola mundo'})
 })
-
 app.use(cors())
+app.use(helmet())
 app.use(morgan('dev'))
 
 app.use('/users',userRoutes)
-app.use('/Products',productsRoutes)
+app.use('/products',productsRoutes)
+app.use('/compras',comprasRoutes)
 
 
 const PORT= process.env.PORT ?? 3010
