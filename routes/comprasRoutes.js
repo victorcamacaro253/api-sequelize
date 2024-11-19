5954748676d96ad11c7dd359e729f100a5f1dde3
Victor C <victorcamacaro253@gmail.com>
2024-11-19 15:53:28 -0400

    Add files via upload

--- routes/comprasRoutes.js
+++ routes/comprasRoutes.js
@@ -3,7 +3,21 @@ import comprasController from "../controllers/comprasController.js";
 
 const router = Router()
 
+//Ruta para obtener  todas las compras
 
 router.get('/',comprasController.getCompras)
+
+
+//Ruta para obtener  las compras de Id de usuario 
+
+router.get('/user/:id',comprasController.getComprasByUserId)
+
+//Ruta para obtener  las compras de Id 
+
+router.get('/:id',comprasController.getComprasById)
+
+//Ruta para agregar una compra
+
+router.post('/',comprasController.compraProduct)
 
 export default router
