26804257b6b59def886197d4ec5de0c60b64e3b0
Victor C <victorcamacaro253@gmail.com>
2024-11-19 15:53:01 -0400

    Add files via upload

--- models/userModel.js
+++ models/userModel.js
@@ -53,7 +53,7 @@
 
 
 // Modelo Usuario
-Usuario.hasMany(Compra, { foreignKey: 'id_usuario' });
+//user.hasMany(Compra, { foreignKey: 'id_usuario' });
 // Relación: Un usuario tiene un rol
 user.belongsTo(Role, { foreignKey: 'rol', as: 'role' });
 
