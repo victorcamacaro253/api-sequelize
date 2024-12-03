eccf7de20cef65d9a950f6036786e5a35a363a43
Victor C <victorcamacaro253@gmail.com>
2024-12-03 15:52:09 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ app.get('/csrftoken',csrfProtection,(req,res)=>{
     res.json({csrfToken:req.csrfToken()})
 })
 
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Ruta no encontrada' });
+  });
 
 
 const PORT= process.env.PORT ?? 3010
