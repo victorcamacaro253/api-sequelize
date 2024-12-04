6bbf03a9a1a8a12e9bd021f7e1bb57f23298ac2f
Victor C <victorcamacaro253@gmail.com>
2024-12-04 11:22:53 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,19 @@ import userRoutes from "./userRoutes.js";
 import productRoutes from "./productsRoutes.js";
 import comprasRoutes from "./comprasRoutes.js";
 import noticationsRoutes from "./notificationsRoutes.js";
-import csrf  from "csurf";
 import cookieParser from "cookie-parser";
+import csrf from '../middleware/csrfToken.js'
+import routeNotFound from "../middleware/routeNotFound.js";
 
 
 const router = Router();
 
-const csrfProtection = csrf({cookie:true})
+
 
 
 router.use(cookieParser())
+
+router.get('/csrftoken',csrf.setCsrfToken)
 
 router.use('/users',userRoutes)
 
@@ -21,7 +24,13 @@ router.use('/auth',authRoutes)
 
 router.use('/products',productRoutes)
 
-router.use('/compras',comprasRoutes)
+router.use('/compras',csrf.csrfMiddleware,comprasRoutes)
 
 router.use('/notificaciones',noticationsRoutes)
+
+
+router.use(routeNotFound);
+
+
+
 export default router;
